9f2001237f83b09211e47787ae189844d50a7e85
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2021-12-20 15:39:24 +0100

    'upload'

--- backend/admin_control.php
+++ backend/admin_control.php
@@ -3,8 +3,11 @@ session_start();
 if (isset($_POST['upload'])) {
 
   include 'dbh.php';
-
+  $msg="avant  .basename($_FILES[video][name]";
+  echo("<script>console.log('PHP: " . $msg . "');</script>");
   $targetvid = "../video-uploads/".basename($_FILES['video']['name']);
+  $msg="apres   .basename($_FILES[video][name]";
+  echo("<script>console.log('PHP: " . $msg . "');</script>");
   $targetimg = "../image-uploads/".basename($_FILES['image']['name']);
   $name = strtolower($_POST['mname']);
   $rdate = $_POST['release'];
@@ -15,8 +18,7 @@ if (isset($_POST['upload'])) {
   $desc = $_POST['desc'];
   $image = $_FILES['image']['name'];
   $video = $_FILES['video']['name'];
-  $msg="image ".$image." video ".$video;
-  echo("<script>console.log('PHP: " . $msg . "');</script>");
+  
   $req = $bdd->prepare("INSERT INTO movies (name, rdate, genre, runtime, description,keywords_en,keywords_fr, imgpath, videopath) VALUES(:name , :rdate , :genre , :run , :desc ,:key_e,:key_f, :img , :video )");
   $req->bindValue('name',$name, PDO::PARAM_STR);
   $req->bindValue("rdate",$rdate, PDO::PARAM_STR );
@@ -28,8 +30,6 @@ if (isset($_POST['upload'])) {
   $req->bindValue('key_f',$key_f, PDO::PARAM_STR);
   $req->bindValue('video',$targetvid, PDO::PARAM_STR);
   $req->execute();
-  $msg="ecris dans la base";
-  echo("<script>console.log('PHP: " . $msg . "');</script>");
 
   if (move_uploaded_file($_FILES['image']['tmp_name'],$targetimg) && move_uploaded_file($_FILES['video']['tmp_name'],$targetvid)) {
     header("Location: https://ayr-streaming.herokuapp.com/homepage.php");
